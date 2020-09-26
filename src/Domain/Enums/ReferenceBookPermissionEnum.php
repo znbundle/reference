8966e6b413f64b6720b8fcd80c87d9e0687bbc4a
Vitaliy <theyamshikov@yandex.ru>
2020-09-26 21:03:44 +0600

    upd

--- src/Domain/Enums/ReferenceBookPermissionEnum.php
+++ src/Domain/Enums/ReferenceBookPermissionEnum.php
@@ -2,15 +2,14 @@
 
 namespace ZnBundle\Reference\Domain\Enums;
 
-use ZnCore\Domain\Base\BaseEnum;
-
-class ReferenceBookPermissionEnum extends BaseEnum
+class ReferenceBookPermissionEnum
 {
 
     const WRITE = 'oReferenceBookWrite';
     const READ = 'oReferenceBookRead';
 
-    public static function getLabels() {
+    public static function getLabels()
+    {
         return [
             self::WRITE => 'Справочник. Модификация',
             self::READ => 'Справочник. Чтение',
