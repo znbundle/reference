d511adb395bf1063be5f338ae5c01b3e61c950b2
Vitaliy <theyamshikov@yandex.ru>
2021-01-02 17:53:18 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
     'ZnBundle\Reference\Domain\Interfaces\Services\BookServiceInterface' => 'ZnBundle\Reference\Domain\Services\BookService',
-    'ZnBundle\Reference\Domain\Interfaces\Repositories\BookRepositoryInterface' => 'ZnBundle\Reference\Domain\Repositories\Eloquent\BookRepository',
     'ZnBundle\Reference\Domain\Interfaces\Services\ItemServiceInterface' => 'ZnBundle\Reference\Domain\Services\ItemService',
+    'ZnBundle\Reference\Domain\Interfaces\Repositories\BookRepositoryInterface' => 'ZnBundle\Reference\Domain\Repositories\Eloquent\BookRepository',
     'ZnBundle\Reference\Domain\Interfaces\Repositories\ItemRepositoryInterface' => 'ZnBundle\Reference\Domain\Repositories\Eloquent\ItemRepository',
-
 ];
