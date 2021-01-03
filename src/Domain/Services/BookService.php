a6993af5822ada9553a40cec404505db802fbf67
Vitaliy <theyamshikov@yandex.ru>
2021-01-03 16:45:26 +0600

    upd

--- src/Domain/Services/BookService.php
+++ src/Domain/Services/BookService.php
@@ -5,15 +5,17 @@ namespace ZnBundle\Reference\Domain\Services;
 use ZnBundle\Reference\Domain\Interfaces\Repositories\BookRepositoryInterface;
 use ZnBundle\Reference\Domain\Interfaces\Services\BookServiceInterface;
 use ZnCore\Domain\Base\BaseCrudService;
+use ZnCore\Domain\Traits\SoftDeleteTrait;
+use ZnCore\Domain\Traits\SoftRestoreTrait;
 
 class BookService extends BaseCrudService implements BookServiceInterface
 {
+
+    use SoftDeleteTrait;
+    use SoftRestoreTrait;
 
     public function __construct(BookRepositoryInterface $repository)
     {
         $this->repository = $repository;
     }
-
-
 }
-
--- src/Domain/Services/ItemService.php
+++ src/Domain/Services/ItemService.php
@@ -5,9 +5,14 @@ namespace ZnBundle\Reference\Domain\Services;
 use ZnBundle\Reference\Domain\Interfaces\Repositories\ItemRepositoryInterface;
 use ZnBundle\Reference\Domain\Interfaces\Services\ItemServiceInterface;
 use ZnCore\Domain\Base\BaseCrudService;
+use ZnCore\Domain\Traits\SoftDeleteTrait;
+use ZnCore\Domain\Traits\SoftRestoreTrait;
 
 class ItemService extends BaseCrudService implements ItemServiceInterface
 {
+
+    use SoftDeleteTrait;
+    use SoftRestoreTrait;
 
     public function __construct(ItemRepositoryInterface $repository)
     {
