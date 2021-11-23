e2ae969ad7ae374da3c9fa08793a352747baf872
Vitaliy <theyamshikov@yandex.ru>
2021-11-23 17:14:02 +0600

    upd

---
+++ src/Domain/Migrations/m_2021_01_19_065433_update_title_column_reference_item_table.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Database\Schema\Blueprint;
+use ZnLib\Migration\Domain\Base\BaseColumnMigration;
+
+class m_2021_01_19_065433_update_title_column_reference_item_table extends BaseColumnMigration
+{
+
+    protected $tableName = 'reference_item';
+
+    public function tableSchema()
+    {
+        return function (Blueprint $table) {
+            $table->string('title', 512)->change();
+        };
+    }
+}
